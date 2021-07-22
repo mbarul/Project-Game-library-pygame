2db71d8f2ad182cb7c0a5a65f25ddbcc68d80738
mbarul <marek.barul28@gmail.com>
2021-07-22 12:23:30 +0200

    added bullets and alien

--- alien_invansion.py
+++ alien_invansion.py
@@ -4,7 +4,8 @@ import pygame
 
 from ship import Ship
 from settings import Settings
-
+from bullet import Bullet
+from alien import Alien
 
 class AlienInvasion:
     """General class for resource management and way the game works"""
@@ -15,11 +16,17 @@ class AlienInvasion:
         self.settings = Settings()
 
         #Wielkość ekranu
-        self.screen= pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.screen= pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Inwazja obców!")
 
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+        self._create_fleet()
         #Kolor tła
         self.bg_color = (self.settings.bg_color)
 
@@ -28,15 +35,17 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets()
             self._update_screen()
 
+            
     def _check_events(self):
         """Reakcja na zdarzenia generowane przez klawiaturę i mysz"""
         #Oczekiwanie na naciśnięcie klawisza lub muszy.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            elif event.type == pygame.KEYDOWN:
+            if event.type == pygame.KEYDOWN:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
@@ -48,18 +57,48 @@ class AlienInvasion:
                 #Przesunięcie statku w lewo
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
     def _check_keyup_events(self, event):   
         """Reakcja na zwolnienie przycisku"""     
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
-    
+
+    def _fire_bullet(self):
+        """Utworzenie nowego pocisku i dodanie go do grupy pocisków"""
+        if len (self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Uaktualnienie położenia pocisków i usunięcie tych niewdicznych na ekranie"""
+        #Uaktualnienie położenia pocisków
+        self.bullets.update()
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)        
+            
+    def _create_fleet(self):
+        """Utworzenie pełnej floty obcych."""
+        #Utworzenie obcego.
+        alien = Alien(self)
+        self.aliens.add(alien)
+
+
     def _update_screen(self):
         """Uaktualnienie obrazów na ekranie i przejście do nowego ekranu."""
         #Odświeżenie ekranu w trakcie każdej iteracji pętli.
         self.screen.fill(self.bg_color)
         self.ship.blitme()
+
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+        self.aliens.draw(self.screen)
         #Wyświetlenie ostatno zmodyfikowanego ekranu
         pygame.display.flip()
 
--- settings.py
+++ settings.py
@@ -7,5 +7,11 @@ class Settings:
         self.screen_height = 640
         self.bg_color = (230,230,230)
 
+        #Ustawienia dotyczące pocisku
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
+        self.bullets_allowed = 3
         #Ustawienie dotyczzące statku
         self.ship_speed = 0.5
